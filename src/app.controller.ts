a8be755d742ce380d697ec6298ed3a7015fd0321
1005jh <kjhhy1005@gmail.com>
2023-02-23 17:26:41 +0900

    interceptor,middleware,exceptionfilter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { SuccessInterceptor } from './interceptors/success.interceptor';
+import { UsersService } from './users/users.service';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+@UseInterceptors(SuccessInterceptor)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log('여기요 여기!!');
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  test(): string {
+    return this.usersService.findAll();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { UsersService } from './users/users.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
